8a85bb06f95d345699e37294e0f2560f5b918504
Tunde Yakub <tundenasri@gmail.com>
2019-07-02 17:28:56 +0100

    set up react router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes/routes';
+
 import './style.css';
 
-const App = () => <div>Hello world</div>;
+const App = () => <Routes />;
 
-ReactDom.render(<App />, document.getElementById('app'));
+ReactDom.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('app'),
+);
