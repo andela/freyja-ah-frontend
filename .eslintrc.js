fda4cef10f5fc5c43336da31f94d4a68b57e475c
Ochowo Ikongbeh <ochowoikongbeh@gmail.com>
2019-07-02 10:53:46 +0100

    chore(react): add more folders [delivers #166236202]

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,18 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
   extends: ['airbnb'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['react'],
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }]
-  }
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
 };
