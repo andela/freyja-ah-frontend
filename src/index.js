bd0e212f94b31d4ca50d22e7500e5011f3a74d2f
Ochowo Ikongbeh <ochowoikongbeh@gmail.com>
2019-07-02 10:53:46 +0100

    chore(react): add more folders [delivers #166236202]

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './style.css';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends React.Component {
-  render() {
-    return <div>Hello world</div>;
-  }
-}
+const App = () => <div>Hello world</div>;
 
 ReactDom.render(<App />, document.getElementById('app'));
