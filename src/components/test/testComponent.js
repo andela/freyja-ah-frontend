bfd08906043cc159f5bfef3eff8824d297527b67
Chizindu David <davidchizindu@gmail.com>
2019-07-03 09:24:02 +0100

    Enabled sass pre-processing [delivers #166236207]

---
+++ src/components/test/testComponent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './testComponent.scss';
+
+const TestComponent = () => (
+  <React.Fragment>
+    <div>
+      <div className="newcomponent">
+        <h2>Customer Service Learning Community</h2>
+        <p>
+          Testing the sass configuration on webpack Testing the sass configuration on webpackTesting the sass configuration on webpack
+          Testing the sass configuration on webpackTesting the sass configuration on webpack
+        </p>
+      </div>
+    </div>
+  </React.Fragment>
+);
+
+export default TestComponent;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 import Home from '../views/home';
 import Login from '../views/login';
 import NotFound from '../views/NotFound';
+import Header from '../components/header';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route path="/login" component={Login} />
+    <Route component={Header} path="/sass-check" />
     <Route component={NotFound} />
   </Switch>
 );
