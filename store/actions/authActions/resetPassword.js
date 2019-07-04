93130dfbc30d9ca65cc7028bc4c1a3f1b1b037ba
beejay1293 <mobolajimatti@Mobolajis-MacBook-Pro.local>
2019-07-04 21:18:50 +0100

    ft(forgot-password-page): user can reset password page
    - set up reset password component
    - set up folder structure
    - set up auth reducer
    - set up test
    
    [Delievers #167065453]

--- store/actions/authActions/resetPassword.js
+++ store/actions/authActions/resetPassword.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/prefer-default-export */
 import axios from 'axios';
 
 export const loading = () => ({
@@ -14,22 +15,16 @@ export const passwordResetError = data => ({
   payload: data,
 });
 
-export const ResetPassword = user => async (dispatch) => {
-  try {
-    dispatch(loading());
+export const ResetPassword = user => (dispatch) => {
+  dispatch(loading());
 
-    const resetPassword = await axios('https://freyja-ah-backend.herokuapp.com/api/users/reset', {
-      method: 'POST',
-      data: { email: user },
-      headers: {
-        accept: 'application/json',
-        'Content-type': 'application/json; charset=UTF-8',
-
-      },
-    });
-
-    dispatch(passwordResetSuccess(resetPassword.data.message));
-  } catch (error) {
-    dispatch(passwordResetError(error.response.data.message));
-  }
+  axios('https://freyja-ah-backend.herokuapp.com/api/users/reset', {
+    method: 'POST',
+    data: { email: user },
+    headers: {
+      accept: 'application/json',
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  }).then(res => dispatch(passwordResetSuccess(res.data.message)))
+    .catch(err => dispatch(passwordResetError(err.response.data.message)));
 };
