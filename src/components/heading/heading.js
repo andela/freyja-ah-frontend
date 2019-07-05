6895617007ecad60447d43ad41ea01953d51a76f
Ochowo Ikongbeh <ochowoikongbeh@gmail.com>
2019-07-05 15:05:54 +0100

    chore(heroku):deploy app to heroku[delivers #167105040]

--- src/components/heading/heading.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './heading.scss';
-
-const heading = ({ title }) => <h2 className="boxx-tex">{title}</h2>;
-heading.propTypes = {
-  title: PropTypes.string.isRequired,
-};
-export default heading;
