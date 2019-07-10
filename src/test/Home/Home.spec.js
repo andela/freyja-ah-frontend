6fabac4af69d0158b9d9d129a68fadf70c726d24
Chisom Onwuchekwa <princechekwas@gmail.com>
2019-07-10 16:25:38 +0100

    rebase and use common components

--- src/test/Home/Home.spec.js
+++ src/test/Home/Home.spec.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-undef */
+/* eslint-disable react/jsx-closing-tag-location */
 import React from 'react';
 import { mount } from 'enzyme';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -6,11 +6,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Home from '../../views/Home/Home';
 
 describe('test Landing Page component', () => {
-  const landingPage = mount(
-    <Router>
-      <Home />
-    </Router>,
-  );
+  const landingPage = mount(<Router>
+    <Home />
+  </Router>);
   it('should ensure that home page exists,  and it includes header and footer components', () => {
     expect(landingPage.find('Header').exists()).toBe(true);
     expect(landingPage.find('Footer').exists()).toBe(true);
--- src/test/NotFound/NotFound.spec.js
+++ src/test/NotFound/NotFound.spec.js
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
-=======
-/* eslint-disable no-undef */
-
->>>>>>> feat(Landing Page):
 import React from 'react';
 import { shallow } from 'enzyme';
 import NotFound from '../../views/NotFound';
 
-<<<<<<< HEAD
 describe('test Not Found Page component', () => {
   it('should render NotFound component correctly', (done) => {
-=======
-describe('test Landing Page component', () => {
-  it('should render Route component correctly', (done) => {
->>>>>>> feat(Landing Page):
     shallow(<NotFound />);
     done();
   });
